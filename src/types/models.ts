d020f529f4b1e718c84f1f89043c8eae51e0146a
eirinhi <eirin.iversen04@gmail.com>
2026-09-22 20:42:32 +0200

    Add subject and Task data models
    Closes #1

---
+++ src/types/models.ts
@@ -0,0 +1,18 @@
+/**
+ * A course/subject the user is studying, used to group tasks.
+ */
+export type Subject = {
+    id: string;
+    name: string;
+};
+
+/**
+ * A single task/to-do item that the user adds to their list of tasks, and is associated with a subject.
+ */
+export type Task = {
+    id: string;
+    title: string;
+    deadline: string;
+    done: boolean;
+    subjectId: string;
+};
