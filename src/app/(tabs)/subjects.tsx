6dd937bff3362aad2ffbe7e7d08490d02219e463
eirinhi <eirin.iversen04@gmail.com>
2026-09-24 14:20:54 +0200

    Set up bottom tab navigation with Home, Subjects, and Profile tabs
    Closes #20

---
+++ src/app/(tabs)/subjects.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Subjects() {
+    return (
+        <View>
+            <Text>Subjects screen coming soon...</Text>
+        </View>
+    );
+}
